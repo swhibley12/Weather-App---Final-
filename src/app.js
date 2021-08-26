164094f7220838a1a83ebaf7258f253f5a4d8897
sarah <swhibley12@gmail.com>
2021-08-26 12:59:55 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ function formatDate(timestamp) {
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tues", "Wed"];
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tues"];
 
   let forecastHTML = `<div class="row">`;
   days.forEach(function (day) {
