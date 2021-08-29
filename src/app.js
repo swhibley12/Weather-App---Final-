5c27935cc534fb46b401b80027b767f66b434c66
sarah <swhibley12@gmail.com>
2021-08-29 10:33:22 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,6 +22,14 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
 
@@ -33,15 +41,21 @@ function displayForecast(response) {
       forecastHTML +
       `
       <div class="col-2">
-        <div class="weather-forecast-date">${forecastDay.dt}</div>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max}° </span>
-          <span class="weather-forecast-temperature-min"> ${forecastDay.temp.min}° </span>
+          <span class="weather-forecast-temperature-max"> ${
+            forecastDay.temp.max
+          }° </span>
+          <span class="weather-forecast-temperature-min"> ${
+            forecastDay.temp.min
+          }° </span>
         </div>
       </div>
   `;
