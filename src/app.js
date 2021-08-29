3596bdc44c95db7199e229a2c71a1038fce96913
sarah <swhibley12@gmail.com>
2021-08-29 18:22:22 -0400

    updated

--- src/app.js
+++ src/app.js
@@ -129,7 +129,7 @@ function changeBackgroundTime() {
     document.body.style.background = "url('images/steve-johnson.jpg')";
   }
   if (hour <= 23 && hour >= 18) {
-    document.body.style.background = "url('images/dark-abstract.jpg')";
+    document.body.style.background = "url('images/evening.jpg')";
   }
 }
 
