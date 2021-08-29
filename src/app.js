74b3644ee47e7e59d3ec9daac9598c62b7d48155
sarah <swhibley12@gmail.com>
2021-08-29 18:02:18 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -129,7 +129,7 @@ function changeBackgroundTime() {
     document.body.style.background = "url('images/sky-night.jpg')";
   }
   if (hour <= 23 && hour >= 18) {
-    document.body.style.background = "url('images/cloudy-sky.jpg')";
+    document.body.style.background = "url('images/steve-johnson.jpg')";
   }
 }
 
